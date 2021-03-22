9cb3a3b01238ffd65587f919cc22f51ad661f2ba
Ziqin Zhu <ziqinzhu@MacBook-Pro.local>
2021-03-21 20:00:10 -0400

    update

--- charts.js
+++ charts.js
@@ -58,30 +58,58 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-    var sampelsArry = data
+    var samplescollection = data.sample;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-    
+    var filteredsamples = samplescollection.filter(function(sampleid){
+      return sampleid = newSample;
+    });
     //  5. Create a variable that holds the first sample in the array.
-
+    var matchedfirstSample = filteredsamples[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var otu_ids_array = data.sample.map(function(sample){
+      return sample.otu_ids;
+    });
+    var otu_labels_array = data.sample.map(function(sample){
+      return sample.otu_labels;
+    });
+    var sample_values_array = data.sample.map(function(sample){
+      return sample.sample_values;
+    });
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
-
+    var yticks = filteredsample.sort((a,b))
     // 8. Create the trace for the bar chart. 
-    var barData = [
+    var barData = [{
+      x : 
+      y : 
+      type: "bar",
+      orientation: 'h',
+    }
       
     ];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      title: "Top 10 Bacteria Cultures Found"
+
     };
     // 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot('charts', barData, barLayout);
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+   
+    ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
     
   });
 }
